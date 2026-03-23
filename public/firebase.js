7ee2c8c4770718a195022886d35320163c60cb5c
Jesus220216 <jjr1602@gmail.com>
2026-03-23 15:55:33 -0400

    firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,14 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig = {
+const app = initializeApp({
   apiKey: "AIzaSyD1w_66STxqf5iMVneB8DgLnpFwS8RGy3g",
   authDomain: "rutarizador-v12.firebaseapp.com",
   projectId: "rutarizador-v12"
-};
-
-const app = initializeApp(firebaseConfig);
+});
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
