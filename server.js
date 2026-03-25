24d5aea8afe9548c6485522ba83999f05a392950
Jesus220216 <jjr1602@gmail.com>
2026-03-25 17:40:12 -0400

    server.js

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.get("/", (req, res) => {
 /* =========================
 🔥 FIREBASE
 ========================= */
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_KEY.replace(/\\n/g, '\n')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
