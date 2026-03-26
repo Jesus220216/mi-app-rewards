4389331e76390d6964e2554b01cc3d72859676a5
Jesus220216 <jjr1602@gmail.com>
2026-03-26 08:57:54 -0400

    firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -3,9 +3,9 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const app = initializeApp({
-  apiKey: "AIzaSyD1w_66STxqf5iMVneB8DgLnpFwS8RGy3g",
-  authDomain: "rutarizador-v12.firebaseapp.com",
-  projectId: "rutarizador-v12"
+  apiKey: "TU_API_KEY_REAL",
+  authDomain: "mi-app-rewards-rd.firebaseapp.com",
+  projectId: "mi-app-rewards-rd"
 });
 
 export const auth = getAuth(app);
